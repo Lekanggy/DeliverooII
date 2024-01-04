bce89f21188f832eb5c7aa00cf714cec5217f89f
lekanggy <lekanggy12@gmail.com>
2024-01-04 15:57:25 +0100

    update part one

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { SearchIconn, ChevronDownIcon, UserIcon, AdjustmentsVerticalIcon } from 'react-native-heroicons/outline'
 
 const Home = () => {
   const navigation = useNavigation()
@@ -14,7 +15,23 @@ const Home = () => {
 
   return (
     <SafeAreaView>
-      <Text className="text-red-500">Home haa i got it</Text>
+      <Text className="text-red-500">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image 
+            source={{uri: "https://links.papareact.com/wru"}}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB"/>
+            </Text>
+          </View>
+        </View>
+       
+      </Text>
     </SafeAreaView>
   )
 }
